5a662d4868ee70daf9fd760f09625bfbfea30c86
triuk <93537202+triuk@users.noreply.github.com>
2026-08-04 19:27:53 +0200

    Add prototype entry point

---
+++ prototype/main.js
@@ -0,0 +1,47 @@
+import { elements, setRunningControls } from "./core.js";
+import { enableManualModes, startCamera, stopCamera } from "./camera.js";
+import {
+  beginRoiDrag,
+  captureDarkSpectrum,
+  clearDarkSpectrum,
+  drawEmptyPlot,
+  drawPlot,
+  endRoiDrag,
+  exportCsv,
+  initialiseDefaultRoi,
+  resizeOverlay,
+  saveFrame,
+  trimSpectrumHistory,
+  updateRoiDrag,
+  useRoiWidthForCalibration,
+} from "./spectrum.js";
+
+function bindEvents() {
+  elements.startButton.addEventListener("click", startCamera);
+  elements.stopButton.addEventListener("click", stopCamera);
+  elements.manualModeButton.addEventListener("click", enableManualModes);
+  elements.captureDarkButton.addEventListener("click", captureDarkSpectrum);
+  elements.clearDarkButton.addEventListener("click", clearDarkSpectrum);
+  elements.useRoiWidthButton.addEventListener("click", useRoiWidthForCalibration);
+  elements.exportCsvButton.addEventListener("click", exportCsv);
+  elements.saveFrameButton.addEventListener("click", saveFrame);
+  elements.overlayCanvas.addEventListener("pointerdown", beginRoiDrag);
+  elements.overlayCanvas.addEventListener("pointermove", updateRoiDrag);
+  elements.overlayCanvas.addEventListener("pointerup", endRoiDrag);
+  elements.overlayCanvas.addEventListener("pointercancel", endRoiDrag);
+  elements.overlayCanvas.addEventListener("dblclick", initialiseDefaultRoi);
+
+  for (const input of [
+    elements.showLuminance, elements.showRed, elements.showGreen, elements.showBlue,
+    elements.subtractDark, elements.pixel1, elements.pixel2,
+    elements.wavelength1, elements.wavelength2,
+  ]) input.addEventListener("input", drawPlot);
+
+  elements.averageFrames.addEventListener("change", trimSpectrumHistory);
+  window.addEventListener("resize", () => { resizeOverlay(); drawPlot(); });
+  window.addEventListener("beforeunload", stopCamera);
+}
+
+bindEvents();
+setRunningControls(false);
+drawEmptyPlot();
