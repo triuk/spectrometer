44da03e875fb860ba7fbda56160bb3e67f3e06d4
triuk <93537202+triuk@users.noreply.github.com>
2026-08-05 13:34:01 +0200

    Load image settings styles

--- prototype/main.js
+++ prototype/main.js
@@ -1,3 +1,4 @@
+import "./image-settings-style.js";
 import { elements, setRunningControls } from "./core.js";
 import { startCamera, stopCamera } from "./camera.js";
 import { installCaptureStatusCorrection } from "./capture-status.js";
