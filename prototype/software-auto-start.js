f5a7388845c642572c7cc2c969da9cc26a99072c
triuk <93537202+triuk@users.noreply.github.com>
2026-08-05 14:57:41 +0200

    Start software optimization after camera is fully ready

--- prototype/software-auto-start.js
+++ prototype/software-auto-start.js
@@ -24,10 +24,7 @@ function poll() {
   if (!startedForTrack && cameraReadyForOptimisation()) {
     startedForTrack = true;
     window.setTimeout(() => {
-      if (state.track === observedTrack
-          && document.documentElement.dataset.softwareAutoExposure !== "active") {
-        elements.autoModeButton.click();
-      }
+      if (state.track === observedTrack) elements.autoModeButton.click();
     }, 250);
   }
 }
